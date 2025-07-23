46daca2b94f1153fd5ed3ef9b898b27d0ff3e467
developeritexpert3@gmail.com <developeritexpert3@gmail.com>
2025-07-23 20:10:35 +0530

    update : ui

--- src/components/Layout/WhyChoose/WhyChoose.tsx
+++ src/components/Layout/WhyChoose/WhyChoose.tsx
@@ -9,7 +9,7 @@ function WhyChoose() {
                     <h6 className='mb-3 font-bold text-sm primary-color'>Why Choose us</h6>
                     <h3 className='font-bold md:text-5xl text-2xl text-[#000] leading-[1.2]'>Built for Growth — From Lead to Close</h3>
                 </div>          
-                <p className='font-light mb-4 ml-6'>At Lead Fusion, we&apos;ve engineered every part of our platform to help  companies win — from the moment a lead comes in, to the final closed deal. Whether you're managing a sales team or scaling across markets, our tools give you total visibility, faster follow-up, and smarter conversions. </p>
+                <p className='font-light mb-4 ml-6'>{`At Lead Fusion, we've engineered every part of our platform to help  companies win — from the moment a lead comes in, to the final closed deal. Whether you're managing a sales team or scaling across markets, our tools give you total visibility, faster follow-up, and smarter conversions.`} </p>
                 <p className='font-light mb-4 ml-6'>We deliver leads, a powerful lead management dashboard, and dedicated support every step of the way — so you can focus on growing your  business with confidence.</p>
                 <button className='bg-black border border-[#000] hover:bg-transparent hover:text-[#000] duration-300 mt-6 px-6 py-3 text-[#fff] rounded-[6px] ml-6'>READ MORE</button>
             </div>
